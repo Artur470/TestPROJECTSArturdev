4d09c3c6bbd36fca71bc3306e3b7e1a7333e7f59
Artur470 <unkulovartur7@gmail.com>
2025-04-25 15:24:30 +0600

    updatre ocde add data basemydb

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default=5432),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
