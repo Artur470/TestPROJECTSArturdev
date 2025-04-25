8758b58e87f4ad27fabd030bb58b8a0739b3892d
Artur470 <unkulovartur7@gmail.com>
2025-04-25 13:40:27 +0600

    update code add .env debug false

--- config/settings.py
+++ config/settings.py
@@ -144,10 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_URL = '/static/'  # URL для статики
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
